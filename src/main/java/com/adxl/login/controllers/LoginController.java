fede6bc59738899f46d9a0a32b6eef50f735b32c
Adel Senhadji <adelsenhadjii@gmail.com>
2020-01-13 20:55:05 +0100

    added login part

--- src/main/java/com/adxl/login/controllers/LoginController.java
+++ src/main/java/com/adxl/login/controllers/LoginController.java
@@ -2,6 +2,7 @@ package com.adxl.login.controllers;
 
 import com.adxl.login.models.User;
 import com.adxl.login.repositories.UsersRepository;
+import org.omg.CosNaming.BindingIterator;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +13,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import javax.validation.Valid;
 
 @Controller
-public class loginController implements WebMvcConfigurer
+public class LoginController implements WebMvcConfigurer
 {
     public UsersRepository usersRepository;
 
-    public loginController(UsersRepository usersRepository)
+    public LoginController(UsersRepository usersRepository)
     {
         this.usersRepository=usersRepository;
     }
@@ -24,20 +25,22 @@ public class loginController implements WebMvcConfigurer
     @Override
     public void addViewControllers(ViewControllerRegistry registry)
     {
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/welcome").setViewName("welcome");
     }
 
-    @GetMapping("/signup")
-    public String showSignUpForm(User user)
+    @GetMapping("/login")
+    public String showLoginForm(User user)
     {
-        return "signup";
+        return "login";
     }
 
-    @PostMapping("signup")
-    public String checkUserInfos(@Valid User user, BindingResult bindingResult)
+    @PostMapping("/login")
+    public String checkCredentials(@Valid User user, BindingResult bindingResult)
     {
         if (bindingResult.hasErrors())
-            return "signup";
-        return "redirect:/login";
+            return "login";
+        return "";
     }
+
+
 }
---
+++ src/main/java/com/adxl/login/controllers/SignupController.java
@@ -0,0 +1,43 @@
+package com.adxl.login.controllers;
+
+import com.adxl.login.models.User;
+import com.adxl.login.repositories.UsersRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.validation.Valid;
+
+@Controller
+public class SignupController implements WebMvcConfigurer
+{
+    public UsersRepository usersRepository;
+
+    public SignupController(UsersRepository usersRepository)
+    {
+        this.usersRepository=usersRepository;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry)
+    {
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    @GetMapping("/signup")
+    public String showSignUpForm(User user)
+    {
+        return "signup";
+    }
+
+    @PostMapping("signup")
+    public String checkUserInfos(@Valid User user, BindingResult bindingResult)
+    {
+        if (bindingResult.hasErrors())
+            return "signup";
+        return "redirect:/login";
+    }
+}
